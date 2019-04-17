1d93245087b8de44d786694e55dbba03a1f39f67
wulinyun <lin.wu@landasoft.com>
2019-04-17 21:16:47 +0800

    调整下依赖暂时少安装一个依赖，拉取资源太慢安装好之后再执行

--- vagrant/scripts/install-rpm-packages.sh
+++ vagrant/scripts/install-rpm-packages.sh
@@ -12,10 +12,10 @@ yum update -y
 yum install -y wget curl tar sudo openssh-server openssh-clients git nodejs npm libuuid-devel libtool zip unzip rsync which erlang cmake bison
 yum install -y bzip2
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install -y erlang
+yum install -y npm erlang
 #ffmpeg
 #yum install -y lumify-videolan-x264 lumify-fdk-aac lumify-lame lumify-opus lumify-ogg lumify-vorbis lumify-vpx lumify-theora lumify-ffmpeg
-/bin/bash /vagrant/vagrant/scripts/install-ffmpeg.sh
+#/bin/bash /vagrant/vagrant/scripts/install-ffmpeg.sh
 
 #tesseract
 #yum install -y lumify-leptonica lumify-tesseract lumify-tesseract-eng
