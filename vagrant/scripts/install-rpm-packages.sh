1cd674e062fe0d9899fbf2aa95b3075a8766ec18
wulinyun <lin.wu@landasoft.com>
2019-04-30 17:29:39 +0800

    新增安装golang依赖用于rabbitmq启动之用

--- vagrant/scripts/install-rpm-packages.sh
+++ vagrant/scripts/install-rpm-packages.sh
@@ -12,7 +12,7 @@ yum update -y
 yum install -y wget curl tar sudo openssh-server openssh-clients git nodejs npm libuuid-devel libtool zip unzip rsync which erlang cmake bison
 yum install -y bzip2
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install -y npm erlang
+yum install -y golang npm
 #ffmpeg
 #yum install -y lumify-videolan-x264 lumify-fdk-aac lumify-lame lumify-opus lumify-ogg lumify-vorbis lumify-vpx lumify-theora lumify-ffmpeg
 #/bin/bash /vagrant/vagrant/scripts/install-ffmpeg.sh
