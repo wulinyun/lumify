f2b080d76d2466324e298dd60415ce0b36d078a4
wulinyun <lin.wu@landasoft.com>
2019-05-23 13:25:33 +0800

    CentOS 6 epel-release放在本地

--- vagrant/scripts/install-rpm-packages.sh
+++ vagrant/scripts/install-rpm-packages.sh
@@ -6,5 +6,5 @@ yum update -y
 #system tools
 yum install -y wget curl tar sudo openssh-server openssh-clients git nodejs npm libuuid-devel libtool zip unzip rsync which erlang cmake bison
 yum install -y bzip2
-rpm -ivh http://apps.k8stest.landaudev.com/lumify/epel-release-6-8.noarch.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum install -y erlang
